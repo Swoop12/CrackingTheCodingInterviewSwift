84ea3e2b434b5c03f2fce87eba2d991e3c422981
Trevor Adcock <trevor.adcock@mac.com>
2022-05-12 22:23:07 -0700

    Initial Commit

---
+++ Algo/AlgoApp.swift
@@ -0,0 +1,17 @@
+//
+//  AlgoApp.swift
+//  Algo
+//
+//  Created by Trevor Adcock on 5/12/22.
+//
+
+import SwiftUI
+
+@main
+struct AlgoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
